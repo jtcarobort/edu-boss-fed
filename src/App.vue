caa0d3ede0d67d3b22ca0f4920348fe4fe379f2a
jiaotiancai <jtc5494@163.com>
2022-07-14 10:12:54 +0800

    目录结构初始化

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    hello
+    <el-row>
+      <el-button disabled>默认按钮</el-button>
+      <el-button type="primary" disabled>主要按钮</el-button>
+      <el-button type="success" disabled>成功按钮</el-button>
+      <el-button type="info" disabled>信息按钮</el-button>
+      <el-button type="warning" disabled>警告按钮</el-button>
+      <el-button type="danger" disabled>危险按钮</el-button>
+    </el-row>
+
+    <el-row>
+      <el-button plain disabled>朴素按钮</el-button>
+      <el-button type="primary" plain disabled>主要按钮</el-button>
+      <el-button type="success" plain disabled>成功按钮</el-button>
+      <el-button type="info" plain disabled>信息按钮</el-button>
+      <el-button type="warning" plain disabled>警告按钮</el-button>
+      <el-button type="danger" plain disabled>危险按钮</el-button>
+    </el-row>
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data () {
+    return {
+      a: 1,
+      b: {
+        c: '4'
+      }
+    }
+  },
+  methods: {
+    test () {
+      console.log(222)
     }
   }
-}
+})
+</script>
+<style lang="scss" scoped>
+
 </style>
